396ebfaec1e0aff7efcab6a23024814336c86196
C00196465 <C00196465@itcarlow.ie>
2018-01-12 01:22:59 +0000

    Shields added + Health Meter

--- AI_Space Station/Player.h
+++ AI_Space Station/Player.h
@@ -11,15 +11,15 @@ public:
 	~Player();
 
 	void update(sf::RenderWindow &w);
-	void updateVelocity(int v);
+	void updateVelocity(float v);
 	void render(sf::RenderWindow &w);
 	void create(sf::Vector2f p, sf::Vector2f vel, std::string file);
 	void setObjRotation(float r);
 	void shoot();
 	sf::Vector2f getVelocity();
 
-	int MAX_SPEED = 2;
-	int MIN_SPEED = 0;
+	float MAX_SPEED = 2.f;
+	float MIN_SPEED = 0;
 	float angle = 20;
 	sf::Vector2f pos;
 
@@ -29,18 +29,23 @@ public:
 	float getRotation();
 
 	std::vector<sf::IntRect> Player::getRects();
+	sf::IntRect Player::getRect();
 	void collisionManager(std::vector<sf::IntRect> r);
-
+	bool isShieldApplied();
+	void applyShield();
 private:
 
 	sf::Vector2f m_velocity;
 	sf::Texture m_texture;
 	sf::Sprite m_spr;
+	sf::Texture m_shieldTexture;
+	sf::Sprite m_shieldSpr;
+	sf::RectangleShape healthMeter;
 
 	std::string m_file;
 	void loadSprite();
 	void boundary(sf::RenderWindow &w);
 	sf::IntRect myBox;
 	int hp = 10;
-
+	bool shieldApplied = false;
 };
